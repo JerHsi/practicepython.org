74d4765bbac3a43eda52ef3da4334c763e1d2398
driftholic <33585835+driftholic@users.noreply.github.com>
2017-11-21 23:05:00 -0800

    Update math.py

--- math.py
+++ math.py
@@ -2,13 +2,20 @@ import random
 
 count = 1
 correct = 0
-num_q = 5
+num_q = 2
 math = ['+', '-']
+
+def question(sign, num1, num2):
+  answer = int(input("\nQuestion #" + str(count) + ":\n " + str(num1) + ' ' + sign + ' ' + str(num2) + " = "))
+  return answer
+
+def is_correct(sign, num1 , num2, answer):
+  return (answer == eval(str(num1) + sign + str(num2)))
 
 while count <= num_q:
   a,b = random.randint(0,20), random.randint(0,10)
   operation = random.choice(math)
-  
+  '''
   if operation == '+':
     ans = int(input("\nQuestion #" + str(count) + ":\n " + str(a) + " + " + str(b) + " = "))
     if ans == a + b:
@@ -17,6 +24,12 @@ while count <= num_q:
     ans = int(input("\nQuestion #" + str(count) + ":\n " + str(a) + " - " + str(b) + " = "))
     if ans == a - b:
       correct += 1
+  ''' 
+  ans = question(operation, a, b)
+  if is_correct(operation, a ,b, ans):
+    correct +=1
+  else:
+    print("**** Wrong Answer!!! ****")
     
   count +=1
 
@@ -37,7 +50,7 @@ elif perc >= 50.0 and perc < 60.0:
 else:
   grade = 'F'
 
-print("\n\n" + "=" * 50 + "\nYou got " + str(correct) + " correct answers out of " + str(count-1) + " questions. \n\nYour grade is " + grade + "!!!!")
+print("\n\n" + "=" * 50 + "\nYou got " + str(correct) + " correct answers out of " + str(count-1) + " questions. \nYou got " + str(num_q - correct) + " wrong answers. \n\nYour grade is *** " + grade + " ***")
 
 
 
