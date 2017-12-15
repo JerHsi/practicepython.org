4f62e19be010fe00328d8abdeef369f39f411d3a
driftholic <33585835+driftholic@users.noreply.github.com>
2017-12-14 16:02:41 -0800

    Update 20.py

--- 20.py
+++ 20.py
@@ -3,18 +3,20 @@ def bSearch(L, num):
   end_index = len(L)-1
   #mid_index = len(L)//2
   
-  
   while end_index - start_index != 0:
     if num < L[start_index] or num > L[end_index]:
       return False
     
-    mid_index = (end_index - start_index) //2
+    mid_index = ((end_index + start_index) //2)
     print(mid_index)
+    print(start_index)
+    print(end_index)
+    print("------")
     
-    if L[mid_index] == num:
+    if L[mid_index] == num or L[start_index] == num or L[end_index] == num:
       return True
       #break
-    elif L[mid_index] > num:
-      end_index = mid_index
-    elif L[mid_index] < num:
-      start_index = mid_index+1
+    elif num < L[mid_index]:
+      end_index = mid_index - 1
+    elif num > L[mid_index]:
+      start_index = mid_index + 1
